656860a65e972cd55fad3f8dbce56e2d60ce7b16
AidenFlissCCSU <110407713+AidenFlissCCSU@users.noreply.github.com>
2022-08-04 13:57:07 -0500

    Minor code change to food detector

--- Assets/FoodEatTrig.cs
+++ Assets/FoodEatTrig.cs
@@ -14,8 +14,11 @@ public class FoodEatTrig : MonoBehaviour
 
     void OnTriggerEnter(Collider obj)
     {
+        Debug.Log("unknown obj touch");
+        Debug.Log("obj name: " + obj.gameObject.name);
         if (obj.gameObject.tag == "Food" && cooldown == false)
         {
+            Debug.Log("object identified!");
             Food foodScr = obj.gameObject.GetComponent< Food >();
             foodScr.TakeBite();
             cooldown = true;
