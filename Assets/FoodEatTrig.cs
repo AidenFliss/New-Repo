4a7043015c74c88f0b0fa6112ddeb0bf68ec05d6
AidenFlissCCSU <110407713+AidenFlissCCSU@users.noreply.github.com>
2022-08-04 13:59:09 -0500

    Update FoodEatTrig.cs

--- Assets/FoodEatTrig.cs
+++ Assets/FoodEatTrig.cs
@@ -6,6 +6,7 @@ public class FoodEatTrig : MonoBehaviour
 {
     public BoxCollider boxCollider;
     bool cooldown = false;
+    Food foodScr;
 
     void Awake()
     {
@@ -19,7 +20,7 @@ public class FoodEatTrig : MonoBehaviour
         if (obj.gameObject.tag == "Food" && cooldown == false)
         {
             Debug.Log("object identified!");
-            Food foodScr = obj.gameObject.GetComponent< Food >();
+            foodScr = obj.gameObject.GetComponent< Food >();
             foodScr.TakeBite();
             cooldown = true;
             DelayAction(0.3f);
