b5227b04b472506bd26eb85fff2151a7685ecc52
AidenFlissCCSU <110407713+AidenFlissCCSU@users.noreply.github.com>
2022-08-05 13:20:13 -0500

    Prototype for drinks

---
+++ Assets/DrinkPourer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkPourer : MonoBehaviour
+{
+    public bool infinite = true;
+    public float rotThreshhold = 120;
+
+    public float origRate = 20.0f;
+
+    public ParticleSystem particles;
+
+    void Update()
+    {
+        if ((transform.rotation.z >= rotThreshhold && transform.rotation.z < 240) || (transform.rotation.x >= rotThreshhold && transform.rotation.x < 240))
+        {
+            Debug.Log("e passes");
+            var em = particles.emission;
+            em.rateOverTime = origRate;
+        }else
+        {
+            Debug.Log("no passe :(");
+            var em = particles.emission;
+            em.rateOverTime = 0f;
+        }
+    }
+}
