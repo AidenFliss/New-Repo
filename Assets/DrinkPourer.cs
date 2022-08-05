f22f7246cab53e05bc67429a7ad38091abb6094b
AidenFlissCCSU <110407713+AidenFlissCCSU@users.noreply.github.com>
2022-08-05 13:42:57 -0500

    Fixed drinks

--- Assets/DrinkPourer.cs
+++ Assets/DrinkPourer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class DrinkPourer : MonoBehaviour
 {
@@ -10,14 +11,19 @@ public class DrinkPourer : MonoBehaviour
     public float origRate = 20.0f;
 
     public ParticleSystem particles;
+    public XRSocketInteractor socket;
 
     void Update()
     {
-        if ((transform.rotation.z >= rotThreshhold && transform.rotation.z < 240) || (transform.rotation.x >= rotThreshhold && transform.rotation.x < 240))// || (transform.rotation.z <= rotThreshhold && transform.rotation > )) //note to future self: loook up checking if angles do thnig or whatavr (or if up is down)
+        //if ((transform.rotation.z >= rotThreshhold && transform.rotation.z < 240) || (transform.rotation.x >= rotThreshhold && transform.rotation.x < 240))// || (transform.rotation.z <= rotThreshhold && transform.rotation > )) //note to future self: loook up checking if angles do thnig or whatavr (or if up is down)
+        if (Vector3.Dot(transform.up, Vector3.down) > 0)
         {
-            Debug.Log("e passes");
-            var em = particles.emission;
-            em.rateOverTime = origRate;
+            if (socket.hasSelection == false)
+            {
+                Debug.Log("e passes");
+                var em = particles.emission;
+                em.rateOverTime = origRate;
+            }
         }else
         {
             Debug.Log("no passe :(");
